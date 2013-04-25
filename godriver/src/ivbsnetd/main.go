5ed97de8c1e24e9efc644933ed5e2fa6d848aafa
Henrik Hugo <hhugo@student.chalmers.se>
2013-04-25 18:43:13 +0200

    Change: Use of iota and added some server functionality

--- godriver/src/ivbsnetd/main.go
+++ godriver/src/ivbsnetd/main.go
@@ -18,6 +18,7 @@ import(
 	"flag"
 	"syscall"
 	"os"
+	"unsafe"
 )
 
 // Capitalized, hush hush
@@ -28,22 +29,53 @@ const (
 
 // Values imported from nbd.h
 const (
-	NBD_SET_SOCK	= 0
-	NBD_SET_BLKSIZE = 1
-	NBD_SET_SIZE	= 2
-	NBD_DO_IT		= 3
-	NBD_CLEAR_SOCK	= 4
-	NBD_CLEAR_QUE	= 5
-	NBD_PRINT_DEBUG	= 6
-	NBD_SET_SIZE_BLOCKS	= 7
-	NBD_DISCONNECT	= 8
-	NBD_SET_TIMEOUT	= 9
-	NBD_SET_FLAGS	= 10
+	NBD_SET_SOCK	= iota
+	NBD_SET_BLKSIZE
+	NBD_SET_SIZE
+	NBD_DO_IT
+	NBD_CLEAR_SOCK
+	NBD_CLEAR_QUE
+	NBD_PRINT_DEBUG
+	NBD_SET_SIZE_BLOCKS
+	NBD_DISCONNECT
+	NBD_SET_TIMEOUT
+	NBD_SET_FLAGS
+)
+
+const (
+	NBD_CMD_READ	= iota
+	NBD_CMD_WRITE
+	NBD_CMD_DISC
+	NBD_CMD_FLUSH
+	NBD_CMD_TRIM
+)
+
+const (
+	NBD_REQUEST_MAGIC	= 0x25609513
+	NBD_REPLY_MAGIC		= 0x67446698
 )
 
 const DATASIZE = 1024*1024*50
 
-func nbd_request(request int) int {
+type nbd_request struct {
+	magic uint32
+	cmd uint32
+	handle [8]byte
+	from uint64
+	len uint32
+}
+
+type nbd_reply struct {
+	magic uint32
+	error uint32
+	handle [8]byte
+}
+
+func ntohl(v uint32) uint32 {
+	return uint32(byte(v >> 24)) | uint32(byte(v >> 16))<<8 | uint32(byte(v >> 8))<<16 | uint32(byte(v))<<24
+}
+
+func request(request int) int {
 	return int(C.nbd_request(C.int(request)))
 }
 
@@ -54,17 +86,37 @@ func ioctl(a1, a2, a3 int) error {
 
 func client(socket_fd, nbd_fd int) {
 	
-	if err := ioctl(nbd_fd, nbd_request(NBD_SET_SOCK), socket_fd); err != nil {
+	if err := ioctl(nbd_fd, request(NBD_SET_SOCK), socket_fd); err != nil {
 		fmt.Println(err)
 	}
 	
-	if err := ioctl(nbd_fd, nbd_request(NBD_DO_IT), 0); err != nil {
+	if err := ioctl(nbd_fd, request(NBD_DO_IT), 0); err != nil {
 		fmt.Println(err)
 	}
 	
-	ioctl(nbd_fd, nbd_request(NBD_CLEAR_QUE), 0)
-	ioctl(nbd_fd, nbd_request(NBD_CLEAR_SOCK), 0)
+	ioctl(nbd_fd, request(NBD_CLEAR_QUE), 0)
+	ioctl(nbd_fd, request(NBD_CLEAR_SOCK), 0)
 	
+}
+
+func server(socket_fd int) {
+	request := new(nbd_request)
+	reply := new(nbd_reply)
+	_ = reply
+	_ = request
+	b := make([]byte, unsafe.Sizeof(request))
+	
+	//fmt.Println(unsafe.Sizeof(*request))
+	
+	for {
+		_, _ = syscall.Read(socket_fd, b)
+		//copy(reply.handle, request.handle)
+		
+		len := ntohl(request.len)
+		_ = len
+		
+		break
+	}
 }
 
 func main() {
@@ -89,7 +141,14 @@ func main() {
 		os.Exit(0)
 	}
 	
+	ioctl(nbd_fd, request(NBD_SET_SIZE), DATASIZE)
+	ioctl(nbd_fd, request(NBD_CLEAR_SOCK), 0)
+	
+	// Dat thread
+	go server(nbd_fd)
 	go client(fd[CLIENT_SOCKET], nbd_fd)
+	
+	
 	
 	syscall.Close(fd[0])
 	syscall.Close(fd[1])
