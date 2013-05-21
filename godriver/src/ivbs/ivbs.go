109b87fee89f39b366d1cbb5c7f2465e9c102800
Henrik Hugo <hhugo@student.chalmers.se>
2013-05-21 15:24:44 +0200

    Add: Correctly formated login packets

--- godriver/src/ivbs/ivbs.go
+++ godriver/src/ivbs/ivbs.go
@@ -1,6 +1,8 @@
 package ivbs
 
 import "encoding/binary"
+import "crypto/sha512"
+import "fmt"
 
 const (
 	OP_GREETING		uint32 = 1
@@ -38,22 +40,37 @@ type IvbsLogin struct {
 }
 const LEN_USERNAME      = 32
 const LEN_PASSWORD_HASH = 128
+const (
+	LEN_SESSIONID     = 32
+	LEN_OP            = 4
+	LEN_STATUS        = 4
+	LEN_DATALEN       = 4
+	LEN_SEQ           = 4
+	LEN_PACKET_HEADER = LEN_SESSIONID + LEN_OP + LEN_STATUS + LEN_DATALEN + LEN_SEQ
+)
 
-type IvbsPacket struct {
-	SessionId [32]byte
+type Packet struct {
+	SessionId []byte
 	Op uint32
 	Status uint32
-	DataLength uint32
+	DataLen uint32
 	Sequence uint32
+	DataSlice []byte
 }
 
-func IvbsStructToSlice(packet *IvbsPacket) ([]byte) {
+func NewPacket() (packet *Packet) {
+	packet = new(Packet)
+	packet.SessionId = make([]byte, LEN_SESSIONID)
+	return packet
+}
+
+func IvbsStructToSlice(packet *Packet) ([]byte) {
 	data := make([]byte, LEN_HEADER_PACKET)
 	
 	copy(data[:32], packet.SessionId[:])
 	binary.BigEndian.PutUint32(data[32:36], packet.Op)
 	binary.BigEndian.PutUint32(data[36:40], packet.Status)
-	binary.BigEndian.PutUint32(data[40:44], packet.DataLength)
+	binary.BigEndian.PutUint32(data[40:44], packet.DataLen)
 	binary.BigEndian.PutUint32(data[44:48], packet.Sequence)
 	
 	return data
@@ -62,19 +79,25 @@ func IvbsStructToSlice(packet *IvbsPacket) ([]byte) {
 func LoginStructToSlice(packet *IvbsLogin) ([]byte) {
 	data := make([]byte, LEN_USERNAME + LEN_PASSWORD_HASH)
 	
+	c := sha512.New()
+	c.Write([]byte(packet.PasswordHash))
+	str := fmt.Sprintf("%x", c.Sum(nil))
+	
 	copy(data[:LEN_USERNAME], []byte(packet.Name))
-	copy(data[LEN_USERNAME:], []byte(packet.PasswordHash))
+	copy(data[LEN_USERNAME:], []byte(str))
+	//copy(data[LEN_USERNAME:], packet.PasswordHash)
 	
 	return data
 }
 
-func IvbsSliceToStruct(data []byte) (*IvbsPacket) {
-	packet := new(IvbsPacket)
+func IvbsSliceToStruct(data []byte) (*Packet) {
+	packet := new(Packet)
+	packet.SessionId = make([]byte, LEN_SESSIONID)
 	
 	copy(packet.SessionId[:], data[:32])
 	packet.Op = binary.BigEndian.Uint32(data[32:36])
 	packet.Status = binary.BigEndian.Uint32(data[36:40])
-	packet.DataLength = binary.BigEndian.Uint32(data[40:44])
+	packet.DataLen = binary.BigEndian.Uint32(data[40:44])
 	packet.Sequence = binary.BigEndian.Uint32(data[44:48])
 	
 	return packet
