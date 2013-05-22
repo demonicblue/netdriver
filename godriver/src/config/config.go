556e7b17cc197c726056355d252524f64b9104d2
LoLip0op <lolip0op@hotmail.se>
2013-05-22 16:58:00 +0200

    Add: Added functionality to config, still WIP. TODO, add real functionality to SetupConnction in nethandler-package.

--- godriver/src/config/config.go
+++ godriver/src/config/config.go
@@ -1,3 +1,40 @@
 package config
 
-// trololo
+import (
+	"io/ioutil"
+	"fmt"
+	"httpserver"
+	"encoding/json"
+	"net/url"
+	"net/http"
+)
+
+var configFile = "/home/alexander/netdriver/godriver/src/config/config.txt"
+
+func ReadFile(){
+	m := httpserver.JSONStruct{}
+	b, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	_ = json.Unmarshal(b, &m)
+	for _, value := range m.Mounted{
+		values := make(url.Values)
+		values.Set("command", "mount")
+		values.Set("nbd", value.NbdDevice)
+		values.Set("target", value.ImageName)
+
+		resp, err := http.PostForm("http://localhost:8080/", values)
+		
+		if err != nil{
+			fmt.Println("Error: ", err)
+		}
+		
+		length := resp.ContentLength
+		temp := make([]byte, length)
+		resp.Body.Read(temp)
+		fmt.Print(string(temp))
+		fmt.Println("------------------------------------------------")
+	}
+}
