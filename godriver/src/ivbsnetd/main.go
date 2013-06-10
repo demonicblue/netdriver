76a8cdd3be2124be3a39111abc8fe336f1c780de
Henrik Hugo <hhugo@student.chalmers.se>
2013-06-10 15:20:21 +0200

    Fix: Removed commented code and formated code with go fmt

--- godriver/src/ivbsnetd/main.go
+++ godriver/src/ivbsnetd/main.go
@@ -1,29 +1,11 @@
 package main
 
-import(
-	"fmt"
+import (
 	"flag"
-	//"syscall"
-	//"os"
-	"time"
-	//"nbd"
-	//"net"
-	//"ivbs"
-	//"net/http"
-	//"strconv"
-	//"strings"
-	"httpserver"
+	"fmt"
+	//"time"
 	"config"
-)
-
-// Capitalized, hush hush
-const (
-	SERVER_SOCKET = 0
-	CLIENT_SOCKET = 1
-)
-
-const (
-	NETD_DISC	= iota
+	"httpserver"
 )
 
 var lista map[int]string
@@ -31,7 +13,7 @@ var listm map[string]string
 
 // Switch byte-order
 func ntohl(v uint32) uint32 {
-	return uint32(byte(v >> 24)) | uint32(byte(v >> 16))<<8 | uint32(byte(v >> 8))<<16 | uint32(byte(v))<<24
+	return uint32(byte(v>>24)) | uint32(byte(v>>16))<<8 | uint32(byte(v>>8))<<16 | uint32(byte(v))<<24
 }
 
 func main() {
@@ -39,23 +21,24 @@ func main() {
 	var nbd_path, server string
 	var nrDevices int
 	var testUser, testPasswd string
-	
+
 	// Setup flags
 	flag.StringVar(&testUser, "u", "", "Username")
 	flag.StringVar(&testPasswd, "p", "", "Password")
-	
+
 	flag.StringVar(&nbd_path, "n", "/dev/nbd5", "Path to NBD device")
 	flag.StringVar(&server, "c", ":8080", "Address for the HTTP-Server")
 	flag.IntVar(&nrDevices, "d", 50, "Number of NBD-devices")
 	flag.Parse()
-	
 
 	// Dat thread
-	//go client(nbd_fd, fd[CLIENT_SOCKET])
-	//fmt.Println("Server..")
-	//go server(fd[SERVER_SOCKET], quitCh, nbd_path, nbd_file)
-	//setupConnection("exjobb-test", testUser, testPasswd, nbd_path)
-	
+	/*
+		go client(nbd_fd, fd[CLIENT_SOCKET])
+		fmt.Println("Server..")
+		go server(fd[SERVER_SOCKET], quitCh, nbd_path, nbd_file)
+		setupConnection("exjobb-test", testUser, testPasswd, nbd_path)
+	*/
+
 	httpAlive := httpserver.SetupHttp(server, nrDevices)
 
 	config.ReadFile()
@@ -64,15 +47,7 @@ func main() {
 
 	fmt.Println("HTTP-Server shutting down...")
 
-	time.Sleep(5 * time.Second)
-	
 	//disconnect(nbd_path, nbd_fd)
-	
-	time.Sleep(5 * time.Second)
-	fmt.Println("Starting ending of main")
-	//syscall.Close(fd[0])
-	//syscall.Close(fd[1])
-	//syscall.Close(nbd_fd)
-	
+
 	fmt.Println("Ending main")
 }
